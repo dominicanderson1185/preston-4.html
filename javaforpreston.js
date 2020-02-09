366df91b9b4af4f0a298dad6a5cf04c818837d8e
xevious <xeviousgreen@gmail.com>
2020-02-08 21:44:20 -0700

    update

--- javaforpreston.js
+++ javaforpreston.js
@@ -4,3 +4,14 @@ hambutton.addEventListener("click", toggleMenu, false);
 function toggleMenu() {
   document.querySelector(".controlbar").classList.toggle("responsive");
 }
+var now = new Date();
+var dayOfWeek = now.getDay();
+if (dayOfWeek != 5) {
+    document.getElementById("announcement").style.display = "none";
+}
+
+var now = new Date();
+var dayOfWeek = now.getDay();
+if (dayOfWeek != 5) {
+    document.getElementById("announcement").style.display = "none";
+}
